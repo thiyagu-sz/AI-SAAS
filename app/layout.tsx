5d5ec695cb3d66b94e8171fbd78b40e859203db5
thiyagu-sz <thiyaguai2004@gmail.com>
2026-01-13 07:02:10 +0530

    cc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -29,6 +30,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.variable} font-display antialiased`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
